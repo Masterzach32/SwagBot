a3d1200edecc767b937cdcf5f1f74222ad36d0cc
Zach Kozar <zachkozar@masterzach32.net>
2016-08-28 23:22:46 -0400

    Removed BotConfig and Constants from .gitignore

---
+++ discord-music-bot/src/main/java/net/masterzach32/discord_music_bot/utils/BotConfig.java
@@ -0,0 +1,99 @@
+package net.masterzach32.discord_music_bot.utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import com.google.gson.Gson;
+
+public class BotConfig implements JSONReader {
+	
+	private String discordAuthKey;
+	private int volume, skipCounter;
+	private boolean queueEnabled, clearCacheOnShutdown;
+	
+	private static final String fileName = "prefs.json";
+
+	/**
+	 * Creates a reference to the preferences file, and generates one if it doesn't exist.
+	 * @throws IOException 
+	 */
+	public BotConfig() throws IOException {
+		// defaults
+		volume = 50;
+		skipCounter = 3;
+		discordAuthKey = "";
+		queueEnabled = true;
+		clearCacheOnShutdown = false;
+		
+		File prefs = new File(fileName);
+		if(!prefs.exists()) {
+			prefs.createNewFile();
+			save();
+		}
+	}
+	
+	public void save() {
+		BufferedWriter fout = null;
+		try {
+			fout = new BufferedWriter(new FileWriter(fileName));
+			fout.write(new Gson().toJson(this));
+			fout.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void load() {
+		RandomAccessFile fin = null;
+		byte[] buffer = null;
+		
+		try {
+			// File optionsFile = new File(path);
+			fin = new RandomAccessFile(fileName, "r");		// "r" = open file for reading only
+			buffer = new byte[(int) fin.length()];
+			fin.readFully(buffer);
+			fin.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String json = new String(buffer);
+		BotConfig file = new Gson().fromJson(json, BotConfig.class);
+		volume = file.getVolume();
+		skipCounter = file.getSkipCounter();
+		discordAuthKey = file.getDiscordAuthKey();
+		queueEnabled = file.isQueueEnabled();
+		clearCacheOnShutdown = file.clearCacheOnShutdown();
+	}
+	
+	public String getDiscordAuthKey() {
+		return discordAuthKey;
+	}
+	
+	public void setVolume(int vol) {
+		this.volume = vol;
+	}
+	
+	public int getVolume() {
+		return volume;
+	}
+
+	public boolean toggleQueueEnabled() {
+		return queueEnabled = !queueEnabled;
+	}
+	
+	public boolean isQueueEnabled() {
+		return queueEnabled;
+	}
+	
+	public boolean clearCacheOnShutdown() {
+		return clearCacheOnShutdown;
+	}
+	
+	public int getSkipCounter() {
+		return skipCounter;
+	}
+}
---
+++ discord-music-bot/src/main/java/net/masterzach32/discord_music_bot/utils/Constants.java
@@ -0,0 +1,17 @@
+package net.masterzach32.discord_music_bot.utils;
+
+public class Constants {
+
+	// commands
+	public static final String COMMAND_PREFIX = "~";
+	
+	// storage
+	public static final String BINARY_STORAGE = "bin/";
+	public static final String DIRECTORY_STORAGE = "storage/";
+	public static final String AUDIO_CACHE = DIRECTORY_STORAGE + "cache/";
+	public static final String PLAYLIST_CACHE = DIRECTORY_STORAGE + "playlists/";
+	public static final String LOG_STORAGE = DIRECTORY_STORAGE + "logs/";
+	public static final String TEMP_STORAGE = DIRECTORY_STORAGE + "temp/";
+	public static final String BOT_SETTINGS = "";
+	
+}
