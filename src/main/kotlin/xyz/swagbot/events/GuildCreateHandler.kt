b840857020731247d5e3a7ba8424e282f67d66c5
Zach <zachk@vt.edu>
2018-01-25 17:22:36 -0500

    Welcome message should be sent every time SwagBot is added to a guild.

--- src/main/kotlin/xyz/swagbot/events/GuildCreateHandler.kt
+++ src/main/kotlin/xyz/swagbot/events/GuildCreateHandler.kt
@@ -5,6 +5,8 @@ import sx.blah.discord.api.events.IListener
 import sx.blah.discord.handle.impl.events.guild.GuildCreateEvent
 import sx.blah.discord.util.RequestBuffer
 import xyz.swagbot.database.*
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 /*
  * SwagBot - Created on 8/24/17
@@ -21,15 +23,21 @@ import xyz.swagbot.database.*
 object GuildCreateHandler : IListener<GuildCreateEvent> {
 
     override fun handle(event: GuildCreateEvent) {
-        if (!does_guild_entry_exist(event.guild.stringID)) {
-            xyz.swagbot.database.logger.info("Adding new guild to database: ${event.guild.stringID}")
-            create_guild_entry(event.guild)
+        val now = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond()
+        val joined = event.guild.getJoinTimeForUser(event.client.ourUser).atZone(ZoneId.systemDefault()).toEpochSecond()
 
+        if (now - joined < 10) {
             val builder = AdvancedMessageBuilder(event.guild.defaultChannel)
             builder.withContent("Thanks for adding me to your server! If you need help, check out the getting " +
                     "started guide on my website: https://swagbot.xyz/gettingstarted")
             RequestBuffer.request { builder.build() }
         }
+
+        if (!does_guild_entry_exist(event.guild.stringID)) {
+            xyz.swagbot.database.logger.info("Adding new guild to database: ${event.guild.stringID}")
+            create_guild_entry(event.guild)
+        }
+
         event.guild.initializeAutioPlayer()
         event.guild.audioManager.audioProvider = event.guild.getAudioHandler().audioProvider
         logger.info("Guild ${event.guild.name} (${event.guild.stringID}) is ready to start receiving commands.")
